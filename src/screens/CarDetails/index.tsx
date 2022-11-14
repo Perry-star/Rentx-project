4699138dea6ea8fa330ed93d45b25deefc4ac24a
Perry-star <mateus.ramosilva@gmail.com>
2022-11-14 20:33:38 -0200

    first commit

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+
+import {
+    Container,
+    Header,
+    CarImage,
+
+} from './styles';
+
+export function  CarDetails() {
+    return(
+        <Container>
+            <Header>
+                <BackButton onPress={() =>{}} />
+            </Header>
+            <CarImage>
+                <ImageSlider
+                    imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
+                />
+            </CarImage>
+
+        </Container>
+    )
+
+}
